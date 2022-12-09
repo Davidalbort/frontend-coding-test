c5762aec070554f1001dfc4fd3499f036499ac76
Roger David Alba Ortega <99060136+Davidalbort@users.noreply.github.com>
2022-12-09 02:33:16 -0500

    Fix style main of home page

--- app/Layout.tsx
+++ app/Layout.tsx
@@ -2,7 +2,7 @@ import { Profiles } from "../components"
 
 export const Layout = () => {
     return (
-        <div>
+        <div className="mx-4 my-2">
             <Profiles />
         </div>
     )
